2f7cac10eeb6b6d4b3728733186fbe5fb59bd9ab
CestJeanEnGros <151684899+CestJeanEnGros@users.noreply.github.com>
2025-03-11 15:47:01 -0400

    Delete Clash of clans - Light directory

--- Clash of clans - Light/cell.js
+++
@@ -1,9 +0,0 @@
-class Cell {
-  constructor(x, y, occupied) {
-    this.x = x;
-    this.y = y;
-    this.occupied = false;
-    this.piece = null;
-    this.pieceColor = null;
-    }
-  }
--- Clash of clans - Light/piece.js
+++
@@ -1,7 +0,0 @@
-class Piece {
-  constructor(j, i, type, pieceColor) {
-    this.pos = {x: i, y: j};
-    this.type = type;
-    this.pieceColor = pieceColor;
-    }
-  }
